2512d01d1aa184cad431cec0fac266ac429a5a10
shyheb <chiheb_mahjoub1993@yahoo.fr>
2019-11-16 04:28:57 -0800

    User services

--- src/main/java/com/ditraacademy/travelagency/core/Controller.java
+++ src/main/java/com/ditraacademy/travelagency/core/Controller.java
@@ -1,88 +1,39 @@
 package com.ditraacademy.travelagency.core;
 
-import com.ditraacademy.travelagency.utility.ErrorResponseEntity;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class Controller {
 
     @Autowired
-    UserRepository userRepository;
+    private UserServices userServices;
 
     @PostMapping("/user")
     public ResponseEntity<?> createUser(@RequestBody User user){
-
-        if(user.getName() == null)
-            return new ResponseEntity<>(new ErrorResponseEntity("ERROR : Nom is NULL"), HttpStatus.BAD_REQUEST);
-        if(user.getName().length()<3)
-            return new ResponseEntity<>(new ErrorResponseEntity("ERROR : Nom est inférieur a 3"),HttpStatus.BAD_REQUEST);
-        if (user.getAge() == 0)
-            return new ResponseEntity<>(new ErrorResponseEntity("ERROR : Age est null"),HttpStatus.BAD_REQUEST);
-        if (user.getAge()<0)
-            return new ResponseEntity<>(new ErrorResponseEntity("Error : Age est inferieur à 0"),HttpStatus.BAD_REQUEST);
-
-        user=userRepository.save(user);
-        return  new ResponseEntity<>(user,HttpStatus.OK);
+        return userServices.createUser(user);
     }
 
     @GetMapping("/users")
     public List<User> afficheUsers(){
-        return userRepository.findAll();
+        return userServices.afficheUsers();
     }
 
     @GetMapping("/user/{id}")
     public User Afficheuser(@PathVariable int id){
-        Optional<User> userOptional = userRepository.findById(id);
-        User user = userOptional.get();
-        return user;
+        return userServices.Afficheuser(id);
     }
 
     @DeleteMapping("/user/{id}")
     public ResponseEntity<?> DeleteUser(@PathVariable int id){
-        Optional<User> userOptional = userRepository.findById(id);
-        if (userOptional.isPresent()) {
-            userRepository.deleteById(id);
-            return new ResponseEntity<>(userOptional.get(), HttpStatus.OK);
-        }else
-            return new ResponseEntity<>(new ErrorResponseEntity("invalid id"),HttpStatus.BAD_REQUEST);
+        return userServices.DeleteUser(id);
     }
 
     @PutMapping("/user/{id}")
     public ResponseEntity<?> UpdateUser(@PathVariable int id,@RequestBody User updateduser){
-        Optional<User> userOptional = userRepository.findById(id);
-
-        if (!userOptional.isPresent()){
-            return new ResponseEntity<>(new ErrorResponseEntity("ERROR: id invalid"), HttpStatus.BAD_REQUEST);
-        }
-
-        User legencyUser = userOptional.get();
-
-        if (updateduser.getName() != null)
-            if (updateduser.getName().length()<3)
-                return new ResponseEntity<>(new ErrorResponseEntity("ERROR : LENGTH EST < 3 "),HttpStatus.OK);
-            else
-                legencyUser.setName(updateduser.getName());
-
-        if (updateduser.getAge() != 0)
-            if (updateduser.getAge() < 0)
-                return new ResponseEntity<>(new ErrorResponseEntity("ERROR : AGE < 0"),HttpStatus.OK);
-            else
-                legencyUser.setAge(updateduser.getAge());
-
-         userRepository.save(legencyUser);
-         return new ResponseEntity<>(HttpStatus.OK);
-
-
+        return userServices.UpdateUser(id,updateduser);
     }
 
 
--- src/main/java/com/ditraacademy/travelagency/utility/ErrorResponseEntity.java
+++ src/main/java/com/ditraacademy/travelagency/utility/ErrorResponseEntity.java
@@ -2,7 +2,6 @@ package com.ditraacademy.travelagency.utility;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter @AllArgsConstructor
